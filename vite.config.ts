770c04af9505352c9b7519bf77d2e6ff0d8f09fd
Miguel Moreno Molina <99278397+amiguelmoreno@users.noreply.github.com>
2024-04-26 01:08:22 +0200

    remove dts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  /*  resolve: {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
@@ -25,6 +25,6 @@ export default defineConfig({
         },
       },
     },
-  }, */
+  },
   plugins: [react() /* , dts({ rollupTypes: true }) */],
 });
